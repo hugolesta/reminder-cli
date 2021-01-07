7afc89245cb39a3c90dd098b693e20ddee6da1d1
Hugo Lesta <hlesta@edrans.com>
2021-01-07 19:14:01 -0300

    Add new logic on main func

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,6 +1,12 @@
 package client
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/hugolesta/reminder-cli/client"
+)
 
 var (
 	backendURIFlag = flag.String("backend", "http://localhost:8080", "Backend API URL")
@@ -9,5 +15,16 @@ var (
 
 func main() {
 	flag.Parse()
-	client.NewSwitch(*backendURIFlag)
+	s := client.NewSwitch(*backendURIFlag)
+
+	if *helpFlag || len(os.Args) == 1 {
+		s.Help()
+		return
+	}
+
+	err := s.Switch()
+	if err != nil {
+		fmt.Printf("cmd switch error: %s")
+		os.Exit(2)
+	}
 }
