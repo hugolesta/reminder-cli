2b9124d8ff29ac336b18b849cb331777a7a86ad9
Hugo Lesta <hlesta@edrans.com>
2021-01-07 19:13:29 -0300

    Add new Help function

--- client/switch.go
+++ client/switch.go
@@ -40,6 +40,13 @@ func (s Switch) Switch() error {
 	return cmd()(cmdName)
 }
 
+func (s Switch) Help() {
+	var help string
+	for name := range s.commands {
+		help += name + "\t --help\n"
+	}
+	fmt.Printf("Usage of: %s:\n <command [<args>]\n%s", os.Args[0], help)
+}
 func (s Switch) create() func(string) error {
 	return func(cmd string) error {
 		fmt.Println("create reminder...")
